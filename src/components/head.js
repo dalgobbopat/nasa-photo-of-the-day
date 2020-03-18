0b239888b319887668687ca9f51d767342b75377
Patrick Dal Gobbo <juandalgobbo@Juans-MacBook-Air.local>
2020-03-18 17:01:57 -0400

    created my header

---
+++ src/components/head.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Header(){
+    return(
+    <div className = "top-container">
+        <h1>NASA Photo of the Day!</h1>
+    </div>
+    );
+}
+
+export default Header;
